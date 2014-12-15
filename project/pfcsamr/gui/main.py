62d0739bfc7419d0ebdefe1456774f11dccd8e0d
Guillermo Gutiérrez <xiterrex@gmail.com>
2014-12-15 18:33:05 +0100

    click

--- project/pfcsamr/gui/main.py
+++ project/pfcsamr/gui/main.py
@@ -3,12 +3,16 @@ __author__ = 'terrex'
 import kivy
 kivy.require('1.9.0')
 
-
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.popup import Popup
 
 class PfcsamrApp(App):
-    pass
+
+    def on_touch_up_2(self):
+        popup = Popup(title='eyy', content=Label(text='tocado!'))
+        popup.open()
 
 if __name__ == '__main__':
         PfcsamrApp().run()
