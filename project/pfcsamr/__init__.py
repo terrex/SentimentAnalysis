d7e8639c1abebea51f043cec63df7bdbcec42a92
Guillermo Gutiérrez <xiterrex@gmail.com>
2015-01-22 17:59:07 +0100

    testings

--- project/pfcsamr/__init__.py
+++ project/pfcsamr/__init__.py
@@ -1,6 +1,9 @@
 __author__ = 'terrex'
 
+from classifiers import *
 from features import *
 from filters import *
+from models import *
 from parser import *
+from trainers import *
 from types import *
