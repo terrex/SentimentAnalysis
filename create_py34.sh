5146f838395a4277af868f5de56dd7523aec2adb
Guillermo Gutiérrez <xiterrex@gmail.com>
2015-08-07 13:56:35 +0200

    qt 5.5

--- create_py34.sh
+++ create_py34.sh
@@ -1,28 +1,29 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 # Prerequisites:
 # * Python 3.4
-# * Qt 5.4.2
+# * Qt 5.5.0
 
 pyvenv-3.4 --clear py34
 source py34/bin/activate
-pip3 install -U pip
+pip3.4 install -U pip
 
 pushd PyQt
-    tar xzf sip-4.16.8.tar.gz
-    pushd sip-4.16.8
-	    python3 configure.py
+    tar xzf sip-4.16.9.tar.gz
+    pushd sip-4.16.9
+	    python3.4 configure.py
 		make
 		make install
     popd
 
-	tar xzf PyQt-gpl-5.4.2.tar.gz
-	pushd PyQt-gpl-5.4.2
-	    python3 configure.py --qmake ~/Qt5.4.2/5.4/clang_64/bin/qmake
+	tar xzf PyQt-gpl-5.5.tar.gz
+	patch -p0 < PyQt-gpl-5.5-fix-license-typo.diff
+	pushd PyQt-gpl-5.5
+	    python3.4 configure.py --qmake ~/Qt5.5.0/5.5/clang_64/bin/qmake
 		make
 		make install
 	popd
 popd
 
-pip3 install -r project/requirements.txt
-python3 -m nltk.downloader all
+pip3.4 install -r project/requirements.txt
+python3.4 -m nltk.downloader all
