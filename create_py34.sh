c663ea7b3a095b05e991e441852e163e39cc19f6
Guillermo Gutiérrez <xiterrex@gmail.com>
2015-08-21 14:02:15 +0200

    manual de instalación

--- create_py34.sh
+++ create_py34.sh
@@ -6,7 +6,7 @@
 
 pyvenv-3.4 --clear py34
 source py34/bin/activate
-pip3.4 install -U pip
+pip3 install -U pip
 
 pushd PyQt
     tar xzf sip-4.16.9.tar.gz
@@ -19,11 +19,11 @@ pushd PyQt
 	tar xzf PyQt-gpl-5.5.tar.gz
 	patch -p0 < PyQt-gpl-5.5-fix-license-typo.diff
 	pushd PyQt-gpl-5.5
-	    python3.4 configure.py --qmake ~/Qt5.5.0/5.5/clang_64/bin/qmake
+	    python3.4 configure.py --qmake ~/Qt5.5.0/5.5/*/bin/qmake
 		make
 		make install
 	popd
 popd
 
-pip3.4 install -r project/requirements.txt
+pip3 install -r project/requirements.txt
 python3.4 -m nltk.downloader all
